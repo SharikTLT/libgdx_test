b37a8e1a6eb2ea9258f04544076fa2b0e626490d
SharikTLT <shariktlt@gmail.com>
2013-01-08 12:11:26 +0400

    init

---
+++ src/libgdx_test/LIBGDX_test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libgdx_test;
+
+/**
+ *
+ * @author SharikTLT
+ */
+public class LIBGDX_test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
